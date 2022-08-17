7c6bc555dc699255636b57557a64ae0ce70178b8
keijiro_asano <keijirou1992@yahoo.co.jp>
2022-08-17 23:32:19 +0900

    コメントアウト作成

--- app/src/main/java/com/android/example/alum0808/GalleryView.kt
+++ app/src/main/java/com/android/example/alum0808/GalleryView.kt
@@ -20,41 +20,48 @@ import kotlinx.android.synthetic.main.gallery_view.*
 import kotlinx.android.synthetic.main.view_image_item.view.*
 
 class GalleryView: AppCompatActivity() {
-    private val imageUris = mutableListOf<Uri>()
+    //imageUrisに可変リストを作成する
+    val imageUris = mutableListOf<Uri>()
     override fun onCreate(savedInstanceState: Bundle?) {
+        //よくわからない、、、先にやりますということ？
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.gallery_view)
+        //もどる矢印を設置する
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        val helper = DBHelper(this)
+        //gallery_viewを表示する
+        setContentView(R.layout.gallery_view)
+        //titleにTextViewを代入する
         val title = findViewById<TextView>(R.id.title)
+        //MainActivityからalumIdを取得する
         val alumId: Long = intent.getLongExtra("id", 0)
-
+        //DBHelperクラスを呼び出す
+        val helper = DBHelper(this)
+        //alumIdがロング型でないとき
         if (alumId != 0L) {
+            //ヘルパークラスを使ってalumsテーブルにクエリを投げる
             helper.readableDatabase.use { db ->
-                db.query(
-                    "alums",
-                    arrayOf("id","title"),
-                    "id = ?",
-                    arrayOf(alumId.toString()),
-                    null,
-                    null,
-                    null,
-                    "1"
-                )
-                    .use { cursor ->
+                //Where select id,title from alums alums.id = alumId limit 1
+                db.query("alums", arrayOf("id","title"), "id = ?", arrayOf(alumId.toString()), null, null, null, "1")
+                    .use { cursor ->/* cursorは、検索結果の各行の情報にアクセスするためのオブジェクト。*/
+                        //カーソル位置が最初に戻ることに成功したら
                         if (cursor.moveToFirst()) {
+                            //タイトルにカーソルで取得した1番目の要素を代入(alumIdに対応するタイトルを取得)
                             title.setText(cursor.getString(1))
                         }
                     }
             }
         }
 
-        /* RecyclerView */
+        /* applyメソッドでRecyclerViewを実行する */
         recyclerView.apply {
+            //viewのサイズは固定なので、サイズを固定してパフォーマンス向上
             setHasFixedSize(true)
+            //RecyclerViewのlayoutManagerをGridLayoutManagerに設定する、配列数は3
             layoutManager = GridLayoutManager(this@GalleryView, 3)
+            //recyclerViewに可変リストデータ(ImageAdapter)を紐付ける
             adapter = ImageAdapter()
         }
+
+        //cameraボタンの設定
         val camera =
             findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.camera_start)
         camera.setOnClickListener {
@@ -62,18 +69,23 @@ class GalleryView: AppCompatActivity() {
             intent.putExtra("id", alumId)
             startActivity(intent)
         }
-        findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.delete_alum).setOnClickListener {
+        //deleteボタンの設定
+        val delete =
+            findViewById<com.google.android.material.floatingactionbutton.FloatingActionButton>(R.id.delete_alum)
+        delete.setOnClickListener {
             helper.writableDatabase.use {
                     db ->
                 db.delete("alums", "id = ?", arrayOf(alumId.toString()))
                 Toast.makeText(this, "削除しました", Toast.LENGTH_SHORT).show()
             }
+            //MAinActivity画面にもどる
             finish()
         }
     }
+    //Activity表示時にはじめる
     override fun onResume() {
+        //よくわからない、、、先にやりますということ？
         super.onResume()
-
         /*
         READ_EXTERNAL_STORAGEパーミッション取得済みかどうかの確認。
         READ_EXTERNAL_STORAGEは実行時に権限を要求する必要がある。
@@ -91,34 +103,35 @@ class GalleryView: AppCompatActivity() {
         /* パーミッション取得済みなら、画像を読み込む。*/
         loadImages()
     }
+
+    //画像を読み込む
     private fun loadImages() {
+        //imageUrisを削除(初期化する)
         imageUris.clear()
-        val name = "%alum_test%"
+        //MainActivityからalumIdを取得する
         val alumId: Long = intent.getLongExtra("id", 0)
         /* 検索条件の設定。*/
          /* 読み込む列の指定。nullならすべての列を読み込む。*/
         val projection = arrayOf(
-            MediaStore.Images.Media._ID,
-            MediaStore.Images.Media.DATA,
-            MediaStore.Images.Media.WIDTH,
-            MediaStore.Images.Media.HEIGHT,
-            MediaStore.Images.Media.SIZE
+            MediaStore.Images.Media._ID
         )
-        /* 行の絞り込みの指定。nullならすべての行を読み込む。*/
+        /* 行の絞り込みの指定。nullならすべての行を読み込む。PICASA_IDは非推奨*/
         val selection = "${MediaStore.Images.Media.PICASA_ID} = ?"
 
-        /* selectionの?を置き換える引数 */
+        /* selectionの?を置き換える引数 SQLインジェクションのための処理*/
         val selectionArgs = arrayOf(
             alumId.toString()
         )
         /* 並び順。nullなら指定なし。*/
         val sortOrder = MediaStore.Images.Media._ID + " desc"
 
-        /*
-        このサンプルではMediaStore以外のソースコードを極力少なくするため、メインスレッドで実行している。
-        そのため、スクロールが頻繁に固まる。実際のアプリではバックグラウンドスレッドで実行すること。
-        */
+
+        //このサンプルではMediaStore以外のソースコードを極力少なくするため、メインスレッドで実行している。
+        //そのため、スクロールが頻繁に固まる。実際のアプリではバックグラウンドスレッドで実行すること。
+
+        //content配下のURIをクエリで取得する
         contentResolver.query(
+            //select URI
             MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
             projection, selection, selectionArgs, sortOrder
         )?.use { cursor -> /* cursorは、検索結果の各行の情報にアクセスするためのオブジェクト。*/
@@ -136,13 +149,13 @@ class GalleryView: AppCompatActivity() {
         /* 表示更新 */
         recyclerView.adapter?.notifyDataSetChanged()
     }
+
     companion object {
         const val REQUEST_CODE = 1
     }
 
     inner class ImageAdapter: RecyclerView.Adapter<ImageViewHolder>() {
         override fun getItemCount() = imageUris.size
-
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
             val layoutInflater = LayoutInflater.from(parent.context)
             val view = layoutInflater.inflate(R.layout.view_image_item, parent, false)
@@ -153,6 +166,7 @@ class GalleryView: AppCompatActivity() {
             holder.itemView.imageView.setImageURI(imageUris[position])
         }
     }
+
     inner class ImageViewHolder(v: View): RecyclerView.ViewHolder(v)
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item?.itemId){
